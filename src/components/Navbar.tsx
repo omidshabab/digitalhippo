ca7b5e3e2d4fa4ba146ab2213a08239cd3260204
Omid Shabab <omidshababdev@gmail.com>
2023-12-24 08:04:43 -0500

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,13 @@ import { Icons } from "./Icons"
 import NavItems from "./NavItems"
 import { buttonVariants } from "./ui/button"
 import Cart from "./Cart"
+import { getServerSideUser } from "@/lib/payload-utils"
+import { cookies } from "next/headers"
+import UserAccountNav from "./UserAccountNav"
 
-const Navbar = () => {
-
-     const user = null
+const Navbar = async () => {
+     const nextCookies = cookies()
+     const { user } = await getServerSideUser(nextCookies)
 
      return (
           <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -33,7 +36,7 @@ const Navbar = () => {
 
                                              {user ? null : <span className="h-6 w-px bg-gray-200" aria-hidden="true" />}
 
-                                             {user ? <p></p> : <Link href="/sign-up" className={buttonVariants({ variant: "ghost" })}>Create account</Link>}
+                                             {user ? <UserAccountNav user={user} /> : <Link href="/sign-up" className={buttonVariants({ variant: "ghost" })}>Create account</Link>}
 
                                              {user ? <span className="h-6 w-px bg-gray-200" aria-hidden="true" /> : null}
 
