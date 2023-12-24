8a4b2d3b49d71f3d23af60c4c5a548289ffbce1c
Omid Shabab <omidshababdev@gmail.com>
2023-12-24 02:26:23 -0500

    first commit

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -13,9 +13,21 @@ import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/valid
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner"
 import { ZodError } from "zod";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Page = () => {
+     const searchParams = useSearchParams()
+     const router = useRouter()
+     const isSeller = searchParams.get("as") === "seller"
+     const origin = searchParams.get("origin")
+
+     const continueAsSeller = () => {
+          router.push("?as=seller")
+     }
+
+     const continueAsBuyer = () => {
+          router.replace("/sign-in", undefined)
+     }
 
      const {
           register,
@@ -25,29 +37,29 @@ const Page = () => {
           resolver: zodResolver(AuthCredentialsValidator),
      })
 
-     const router = useRouter()
+     const { mutate: signIn, isLoading } =
+          trpc.auth.signIn.useMutation({
+               onSuccess: () => {
+                    toast.success("Signed in successfully")
 
-     const { mutate, isLoading } =
-          trpc.auth.createPayloadUser.useMutation({
-               onError: (err) => {
-                    if (err.data?.code === "CONFLICT") {
-                         toast.error("This email is already in use. Sign in instead")
+                    router.refresh()
 
+                    if (origin) {
+                         router.push(`/${origin}`)
                          return
                     }
 
-                    if (err instanceof ZodError) {
-                         toast.error(err.issues[0].message)
-
+                    if (isSeller) {
+                         router.push("/sell")
                          return
                     }
 
-                    toast.error("Something went wrong. Please try again.")
+                    router.push("/")
                },
-               onSuccess: ({ sentToEmail }) => {
-                    toast.success(`Verification email sent to ${sentToEmail}`)
-
-                    router.push("/verify-email?to=" + sentToEmail)
+               onError: (err) => {
+                    if (err.data?.code === "UNAUTHORIZED") {
+                         toast.error("Invalid email or password.")
+                    }
                }
           })
 
@@ -56,7 +68,7 @@ const Page = () => {
           password,
      }: TAuthCredentialsValidator) => {
           // send data to the server
-          mutate({ email, password })
+          signIn({ email, password })
      }
 
      return <>
@@ -65,7 +77,7 @@ const Page = () => {
                     <div className="flex flex-col items-center space-y-2 text-center">
                          <Icons.logo className="h-20 w-20" />
                          <h1 className="text-2xl font-bold">
-                              Create an account
+                              Sign in to your {isSeller ? "seller" : ""} account
                          </h1>
 
                          <Link
@@ -73,8 +85,8 @@ const Page = () => {
                                    variant: "link",
                                    className: "gap-1.5"
                               })}
-                              href="/sign-in">
-                              Already have an account? Sign-in
+                              href="/sign-up">
+                              Don&apos;t have an account?
                               <ArrowRight className="h-4 w-4" />
                          </Link>
                     </div>
@@ -112,9 +124,34 @@ const Page = () => {
                                         )}
                                    </div>
 
-                                   <Button>Sign up</Button>
+                                   <Button>Sign in</Button>
                               </div>
                          </form>
+
+                         <div className="relative">
+                              <div aria-hidden="true" className="absolute inset-0 flex items-center">
+                                   <span className="w-full border-t" />
+                              </div>
+                              <div className="relative flex justify-center text-xs uppercase">
+                                   <span className="bg-background px-2 text-muted-foreground">or</span>
+                              </div>
+                         </div>
+
+                         {isSeller ? (
+                              <Button
+                                   onClick={continueAsBuyer}
+                                   variant="secondary"
+                                   disabled={isLoading}>
+                                   Continue as customer
+                              </Button>
+                         ) : (
+                              <Button
+                                   onClick={continueAsSeller}
+                                   variant="secondary"
+                                   disabled={isLoading}>
+                                   Continue as seller
+                              </Button>
+                         )}
                     </div>
                </div>
           </div>
